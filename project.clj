c69fb4595a9acc0939ec47cffcbffac4da6a189e
Aaron France <aaron.l.france@gmail.com>
2014-01-29 23:37:22 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject pismo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [awizo "0.0.3"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [clj-yaml "0.4.0"]
+                 [javax.mail/mail "1.4.3"]]
+  :main ^:skip-aot pismo.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/pismo/core.clj
@@ -0,0 +1,55 @@
+(ns pismo.core
+  (:require [clj-yaml.core :as yaml]
+            [awizo.core :as awizo]
+            [clojure.core.async :as async])
+  (:use [clojure.java.shell :only [sh]])
+  (:import [javax.mail.internet MimeMessage])
+  (:import [javax.mail Session])
+  (:gen-class))
+
+
+(def config (yaml/parse-string (slurp "/etc/pismo.yaml")))
+
+(defn read-message [p]
+  (let [content (slurp p)
+        bais    (java.io.ByteArrayInputStream. (.getBytes content))
+        msg     (MimeMessage. (Session/getInstance (java.util.Properties.)) bais)
+        from    (.toString (first (.getFrom msg)))]    
+    (format "From: %s\n Subject: %s" from (.getSubject msg))))
+
+(defn new-email-notify [p]
+  (prn config)
+  (.start (Thread. (fn [] (sh (config :notifier-command) (config :notifier-args)))))
+  (let [email-info (read-message (.toString p))]
+    (sh "notify-send" "-t" "1500" "New mail!: " email-info)))
+
+(defn mailmessage [p]
+  (let [path (awizo/string->path p)]
+    (fn [e]
+      (let [filepath (.context e)]
+        (new-email-notify (.resolve path filepath))))))        
+
+(defn dir-seq [path]
+  (let [dir-contents (file-seq (clojure.java.io/file path))
+        dirs (filter (fn [p] (.isDirectory p)) dir-contents)]
+    (map (fn [d] (.getPath d)) dirs)))
+
+(defn is-maildir? [path]
+  (or (.contains path "cur")
+      (.contains path "tmp")
+      (.contains path "new")))
+
+(defn create-watcher [path]
+  (awizo/attach-handler path (mailmessage path) [awizo/CREATE]))
+
+(defn create-watchers [config]
+  (flatten
+   (doall
+    (for [path (config :paths)]
+      (if (config :recursive)
+        (let [paths (filter is-maildir? (dir-seq path))]
+          (doall (map create-watcher paths)))
+        (create-watcher path))))))
+
+(defn -main [& args]
+  (create-watchers config))
